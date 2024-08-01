2715ae0cd9288af8db35d15429cf0e7a46537d0e
DjonKrd <dj2393@mail.ru>
2024-08-01 13:35:00 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -4,13 +4,14 @@ calls = 0
 def count_calls():
     global calls
     calls += 1
+    return calls
 
 
 def string_info(string):
     count_calls()
     a = string.upper()
     b = string.lower()
-    print(len(string), a, b)
+    return len(string), a, b
 
 def is_contains(string, list_to_search):
     count_calls()
@@ -19,9 +20,18 @@ def is_contains(string, list_to_search):
         if list_to_search[i].lower() == string.lower():
             c = True
     if c:
-        print(True)
+        return True
     else:
-        print(False)
+        return False
+
+
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
 
 
 
